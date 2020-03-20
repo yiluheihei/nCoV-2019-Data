3b5e5d300384ecdcc803a50f0a092498300e655e
yiluheihei <yiluheihei@gmail.com>
2020-03-21 00:54:35 +0800

    system requirement geos and save csv file

--- get_ncov.R
+++ get_ncov.R
@@ -5,4 +5,6 @@ ncov <- ncovmap::get_ncov2(method = "api", latest = FALSE)
 ncov_latest <- ncovmap::get_ncov2(method = "api", latest = TRUE)
 saveRDS(ncov_latest, "ncov_latest.RDS")
 saveRDS(ncov, "ncov.RDS")
+readr::write_csv(data.frame(ncov), path = "ncov.csv")
+readr::write_csv(data.frame(ncov_latest), path = "ncov_latest.csv")
 
